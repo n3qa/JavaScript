e76fcfdb35126b0f5fc79bf85b25d1ed9f7bc354
Nikolay Nikolov <n3qa@abv.bg>
2024-09-23 21:32:30 +0300

    Adding some demo code for adding and deleting item from a list

--- DOM/DomEvents/02. Add-Delete/app.js
+++ DOM/DomEvents/02. Add-Delete/app.js
@@ -1,3 +1,13 @@
 function addItem() {
-    //TODO...
+   const liElement = document.createElement('li');
+   liElement.textContent = document.getElementById('newText').value;
+
+   document.getElementById('items').appendChild(liElement);
+   document.getElementById('newText').value='';
+
+}
+
+function remove(){
+    document.getElementById('li-to-delete');
+    
 }
