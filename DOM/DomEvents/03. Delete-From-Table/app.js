82d209a9ba7acd23338b8f9f125e32b020e975f6
Nikolay Nikolov <n3qa@abv.bg>
2024-09-23 21:47:41 +0300

    Adding some code for deleting intem from table

--- DOM/DomEvents/03. Delete-From-Table/app.js
+++ DOM/DomEvents/03. Delete-From-Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    //read input field value
+    const email = document.querySelector('input[name="email"').value;
+    const rows = Array.from(document.querySelector('tbody tr'));
+
+    let deleted = false;
+    for (let row of rows){
+        if(row.children[1].textContent==email){
+            row.parentNode.removeChildren(row);
+
+            document.getElementById('result').textContent = 'Deleted';
+        }
+    }
+
+    if (deleted!=true){
+        document.getElementById('result').textContent  = 'Not found'
+    }
 }
