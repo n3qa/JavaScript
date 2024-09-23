438dec07493dd500b1e4a285edc84591156756e5
Nikolay Nikolov <n3qa@abv.bg>
2024-09-23 20:05:11 +0300

    Adding Dom Events demo code skeletons

--- DOM/DOM man practice/07. Hell's Kitchen/app.js
+++ DOM/DOM man practice/07. Hell's Kitchen/app.js
@@ -1,8 +1,40 @@
 function solve() {
-   document.querySelector('#btnSend').addEventListener('click', onClick);
+   document.querySelector('#btnSend').
+   addEventListener('click', onClick);
+
+   let input = document.querySelector('#inputs> textarea');
 
    function onClick () {
-      //   TODO:
+      const arr = JSON.parse(input.value);
+
+      let restaurants = {};
+
+     arr.forEach((line) => {
+         const tokens = line.split(' - ');
+         const name = tokens[0];
+         const workersArr = token[1].split(', ');
+
+         let workerrs = [];
+         let averageSalary = 0;
+         let bestSalary = 0;
+        
+         for (const worker of workersArr) {
+            let workerTokens = worker.split(' ');
+            const salary = Number(workerTokens[1]);
+            workersArr.push({
+               name: workerTokens[0];
+               salary;
+            })
+         } 
+
+       } 
+      );
+
+      restaurants[name] = {
+         workers,
+         avarageSalary,
+
+      }
       
    }
 }
