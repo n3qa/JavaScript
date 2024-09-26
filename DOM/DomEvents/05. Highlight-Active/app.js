34c3f27ed5fcbdc752fefc3eae2ff110a0aeb1e0
Nikolay Nikolov <n3qa@abv.bg>
2024-09-26 09:41:31 +0300

    Adding simple JS script for focus element and defocus background not used content

--- DOM/DomEvents/05. Highlight-Active/app.js
+++ DOM/DomEvents/05. Highlight-Active/app.js
@@ -1,3 +1,16 @@
 function focused() {
-    console.log('TODO:...');
+    // select all input fields
+    document.getElementsByTagName('input').array.forEach(i => {
+        i.addEventListner('focus',onFocus);
+        i.addEventListner('blur',onBlur);
+    });
+
+
+    function onFocus(ev){
+        ev.target.parentNode.className()='focused';
+    }
+
+    function onBlur (ev){
+        ev.target.parentNode.className() = 'blured';
+    }
 }
