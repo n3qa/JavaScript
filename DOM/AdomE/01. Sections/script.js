c21aa1712abbdd574b6aa01a5370addfe660688d
Nikolay Nikolov <n3qa@abv.bg>
2024-09-30 10:36:03 +0300

    Adding some Semi-Adv Basic dom mnaipulation

--- DOM/AdomE/01. Sections/script.js
+++ DOM/AdomE/01. Sections/script.js
@@ -1,3 +1,25 @@
 function create(words) {
-   console.log('TODO:...');
+
+   const conent = document.getElementById('content');
+
+   for (let i = 0; i < words.length; i++) {
+      const div = document.createElement('div');
+      const paragrapth = document.createElement('p');
+
+      paragrapth.textContent = words[i];
+      paragrapth.style.display = 'none';
+
+      div.appendChild(paragrapth);
+      conent.appendChild(div);
+      
+   }
+
+   conent.addEventListener('click', function(e){
+      if(e.target.tagName==='DIV' || e.target.tagName ==='P'){
+         const p = e.target.children[0] || e.target;
+         const isVisible = p.style.display === 'block';
+         p.style.display = isVisible ? 'none' : 'block';
+      }
+
+   })
 }
