ff0f95b0805df70ce2cea2ba6d85603346476037
Nikolay Nikolov <n3qa@abv.bg>
2024-09-30 14:10:34 +0300

    Adding some code/decode JS code

--- DOM/AdomE/05. Encode-and-Decode-Messages/app.js
+++ DOM/AdomE/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,34 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+     const textAreas = document.querySelectorAll('textArea');
+    const buttons = document.querySelectorAll('button');
+
+    const elements = {
+        encode: {
+            text: textAreas[0],
+            btn: buttons[0]
+        },
+
+        decode: {
+            text: textAreas[1],
+            btn: buttons[1]
+            }
+    }
+     
+    document.getElementById('main').addEventListener('click', function(e) {
+        if(e.target.tagName !=='BUTON'){
+            return;
+        }
+
+        const type = e.target.textContent.toLowerCase().trim().includes('encode') ? 'encode' : 'decode';
+
+        const message = elements.type.text.value
+        .split('').map[map[type].func];
+
+        map.encode.textarea.value='';
+        map.decode.textarea.value = message;
+        
+
+
+    } )
+
 }
