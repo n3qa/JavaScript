dcf9b1b6145919fa571c821bf7aa13914bb46566
Nikolay Nikolov <n3qa@abv.bg>
2024-10-04 10:12:04 +0300

    Adding simple calc with closure demo

--- DOM/AdomE/17. Simple Calculator/app.js
+++ DOM/AdomE/17. Simple Calculator/app.js
@@ -1,7 +1,29 @@
 function calculator() {
-    // TODO:
+    let firstElm;
+    let secomdElm;
+    let resultElm;
+
+    return {
+        init: (selector1, slector2, resultSelector) =>{
+         firstElm = document.querySelector(selector1);
+         secomdElm = document.querySelector(selector2);
+         resultElm = document.querySelector(resultSelector);
+
+
+        },
+
+        add: ()=>{
+            return resultElm.value = Number(firstElm.value)+Number(secomdElm.value);
+
+        },
+
+        substract: ()=>{
+            return resultElm.value = Number(firstElm.value)-Number(secomdElm.value);
+        }
+    }
 }
 
-
+let obj = calculator();
+obj.init('#num1','#num2','#result')
 
 
